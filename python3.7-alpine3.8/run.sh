3492bdd93ea3d4d768ff7b186a8865ddec7206e6
acnebs <connor@neblett.com>
2019-04-02 11:00:18 -0500

    made configuration much more configurable

--- python3.7-alpine3.8/run.sh
+++ python3.7-alpine3.8/run.sh
@@ -6,5 +6,5 @@ echo "[RUN] preflight ($PREFLIGHT_PATH)"
 . "$PREFLIGHT_PATH"
 
 # start up gunicorn (with meinheld workers)
-echo "[RUN] gunicorn+meinheld (config: $GUNICORN_CONF)"
-exec gunicorn -k egg:meinheld#gunicorn_worker -c "$GUNICORN_CONF" "$APP_MODULE"
+echo "[RUN] gunicorn+meinheld (config: $GUNICORN_CONF_PATH)"
+exec gunicorn -k egg:meinheld#gunicorn_worker -c "$GUNICORN_CONF_PATH" "$PYTHON_APP"
